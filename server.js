fad3ba4cdb019bc41238390f08911f6773707bac
oopwsyua1-hash <oopwsyua1@gmail.com>
2026-04-24 00:01:14 +0300

    Change server log message to Arabic

--- server.js
+++ server.js
@@ -151,5 +151,5 @@ app.post('/upload/cover', upload.single('cover'), async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`السبع الحلبي شغال على ${PORT}`);
 });
